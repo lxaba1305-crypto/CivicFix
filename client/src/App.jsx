1af7ff5c370a8e3a787f9c3bfe47e892b3ca611a
Natalie Vinyu <135601983+NatalieVinyu@users.noreply.github.com>
2026-05-01 19:48:34 +0200

    feat: prepared dataset for dashboard testing and visualization

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,6 @@ import AdminDasboard from './pages/AdminDasboard'
 function App() {
   return (
     <div>
-      <h1 className='text-red-500 bg-black'>CivicFix</h1>
       <AdminDasboard />
 
     </div>
