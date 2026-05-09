b4429163d1a783dd4b86a083b73a7720ea106aee
Lehlohonolo Xaba <lxaba1305@gmail.con>
2026-05-09 20:43:45 +0200

    Added Routes

--- server/index.js
+++ server/index.js
@@ -3,21 +3,27 @@ import cors from "cors";
 import dotenv from "dotenv";
 import bcrypt from "bcrypt";
 import { createClient } from "@supabase/supabase-js";
-
-const PORT = process.env.PORT || 5000;
+import authRoutes from "./Routes/authRoutes.js";
+import reports from "./Routes/report.js";
 
 dotenv.config();
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
-
-const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
 
 app.get("/", (req, res) => {
   res.send("CivicFix API is running");
 });
 
-app.listen(5000, () => {
-  console.log("Server is running on port 5000");
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
+
+app.use("/auth", authRoutes);
+app.use("/reports", reports);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
